43e98b80c7bf09fae7ea292273b5fd231437d0a1
vichi06 <vincent.n@hotmail.fr>
2022-11-28 20:19:06 +0900

    salt token

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'c3aeaa71045f37bf7f1e2db66bc1294b'),
+  },  
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'wruCdFcj6cSgKgvWVqgXww=='),
   },
 });
